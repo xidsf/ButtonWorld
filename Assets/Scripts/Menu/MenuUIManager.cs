a150fc288f68761d51a233f58c320ac562412d36
xidsf <gooderic816053@gmail.com>
2025-09-11 15:57:39 +0900

    UI아이콘 에셋 import

--- Assets/Scripts/Menu/MenuUIManager.cs
+++ Assets/Scripts/Menu/MenuUIManager.cs
@@ -1,14 +1,33 @@
+using DG.Tweening;
 using UnityEngine;
+
+enum UIType
+{
+    None,
+    StageSelect,
+    Settings,
+    Exit
+}
 
 public class MenuUIManager : MonoBehaviour
 {
-    bool isClickButton = false;
+    public float MoveXDistance = 1920f;
+    public float MoveYDistance = 1080f;
 
-    public float MoveDistance = 500f;
-    [SerializeField] private GameObject[] MoveUIObjects;
-    private RectTransform[] MoveUIObjectOriginPos;
+    [SerializeField] private RectTransform TitleRectTrans;
+    private Vector3 titleOriginPos;
+    [SerializeField] private RectTransform StageSelectObj;
+    private Vector3 stageSelectOriginPos;
+    [SerializeField] private RectTransform SettingsRectTrans;
+    private Vector3 settingsOriginPos;
+    [SerializeField] private RectTransform ExitRectTrans;
+    private Vector3 exitOriginPos;
+
+    private float uiMoveTime = 0.5f;
 
     [SerializeField] private GameObject MenuSelectUI;
+
+    UIType frontUI = UIType.None;
 
     private void Start()
     {
@@ -17,33 +36,61 @@ public class MenuUIManager : MonoBehaviour
 
     private void GetOriginPos()
     {
-        var objCount = MoveUIObjects.Length;
-
-        MoveUIObjectOriginPos = new RectTransform[objCount];
-
-        for (int i = 0; i < objCount; i++)
-        {
-            MoveUIObjectOriginPos[i] = MoveUIObjects[i].GetComponent<RectTransform>();
-        }
+        titleOriginPos = TitleRectTrans.localPosition;
+        stageSelectOriginPos = StageSelectObj.localPosition;
+        settingsOriginPos = SettingsRectTrans.localPosition;
+        exitOriginPos = ExitRectTrans.localPosition;
     }
 
     public void OnClickStageSelectButton()
     {
-
-    }
-
-    public void OnClickReturnToTitleButton()
-    {
-
+        if(frontUI != UIType.None) return;
+        frontUI = UIType.StageSelect;
+        TitleRectTrans.DOLocalMoveY(titleOriginPos.x - MoveXDistance, uiMoveTime).SetEase(Ease.InOutBack);
+        StageSelectObj.DOLocalMoveY(stageSelectOriginPos.x - MoveXDistance, uiMoveTime).SetEase(Ease.InOutBack);
     }
 
     public void OnClickSettingsButton()
     {
-
+        if (frontUI != UIType.None) return;
+        frontUI = UIType.Settings;
+        TitleRectTrans.DOLocalMoveX(titleOriginPos.y - MoveYDistance, uiMoveTime).SetEase(Ease.InOutBack);
+        SettingsRectTrans.DOLocalMoveX(settingsOriginPos.y - MoveYDistance, uiMoveTime).SetEase(Ease.InOutBack);
     }
 
     public void OnClickExitButton()
     {
-        
+        if(frontUI != UIType.None) return;
+        frontUI = UIType.Exit;
+        ExitRectTrans.DOLocalMoveY(exitOriginPos.y - MoveYDistance, uiMoveTime).SetEase(Ease.InOutBack);
     }
+
+    public void OnClickReturnButton()
+    {
+        if(frontUI == UIType.None) return;
+        
+        switch (frontUI)
+        {
+            case UIType.StageSelect:
+                StageSelectObj.DOLocalMove(stageSelectOriginPos, uiMoveTime).SetEase(Ease.InOutBack);
+                TitleRectTrans.DOLocalMove(titleOriginPos, uiMoveTime).SetEase(Ease.InOutBack);
+                break;
+            case UIType.Settings:
+                SettingsRectTrans.DOLocalMove(settingsOriginPos, uiMoveTime).SetEase(Ease.InOutBack);
+                TitleRectTrans.DOLocalMove(titleOriginPos, uiMoveTime).SetEase(Ease.InOutBack);
+                break;
+            case UIType.Exit:
+                ExitRectTrans.DOLocalMove(exitOriginPos, uiMoveTime).SetEase(Ease.InOutBack);
+                break;
+        }
+
+        frontUI = UIType.None;
+
+    }
+
+    public void OnClickQuitButton()
+    {
+        Application.Quit();
+    }
+
 }
